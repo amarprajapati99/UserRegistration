25f2894a924bccd19f6a9954e96b66fcdaa409e7
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-30 00:05:14 +0530

    Enter valid your last name

--- src/com/userregistraction/bridgelabz/UserRegistration.java
+++ src/com/userregistraction/bridgelabz/UserRegistration.java
@@ -9,16 +9,28 @@ public class UserRegistration {
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("enter your first name");
-		String firstName = scanner.next();
+		String firstName = scanner.nextLine();
+
 		Pattern pattern = Pattern.compile("[A-Z]{1}[a-z]{2}+");
 		Matcher matcher = pattern.matcher(firstName);
 		boolean matches = matcher.find();
 		if (matches)
+			System.out.println("your first name is valid");
+		else if (firstName.length() <= 3)
+			System.out.println("enter max 4 character");
+		else
+			System.out.println("your  last name is invalid");
+		System.out.println("enter your last name");
+		
+		
+		String lastName = scanner.nextLine();
+		if (matches)
 			System.out.println("your name is valid");
-		else if(firstName.length()<=3)
+		else if (lastName.length() <= 3)
 			System.out.println("enter max 4 character");
 		else
 			System.out.println("your name is invalid");
+
 	}
 
 }
